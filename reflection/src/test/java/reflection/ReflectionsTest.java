7735e2613d676c41362a22b56942420e92e70cf9
leokim <kunga077@gmail.com>
2022-09-16 16:57:44 +0900

    feat: 4단계 완료

--- reflection/src/test/java/reflection/ReflectionsTest.java
+++ reflection/src/test/java/reflection/ReflectionsTest.java
@@ -1,5 +1,10 @@
 package reflection;
 
+import annotation.Controller;
+import java.lang.annotation.Annotation;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
@@ -12,7 +17,18 @@ class ReflectionsTest {
     @Test
     void showAnnotationClass() throws Exception {
         Reflections reflections = new Reflections("examples");
+        Reflections annotationReflections = new Reflections("annotation");
+        List<Class<? extends Annotation>> subTypesOfAnnotation = annotationReflections.getSubTypesOf(Annotation.class)
+                .stream()
+                .filter(c -> !"Inject".equals(c.getSimpleName()))
+                .collect(Collectors.toList());
 
         // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+        subTypesOfAnnotation.forEach(annotation -> logEachAnnotation(annotation, reflections));
+    }
+
+    private void logEachAnnotation(Class<? extends Annotation> annotation, Reflections reflections) {
+        reflections.getTypesAnnotatedWith(annotation)
+                .forEach(c -> log.info(String.format("@%s annotated to %s", annotation.getSimpleName(), c.getSimpleName())));
     }
 }
