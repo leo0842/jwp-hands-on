88f14409041959c0c8d21496d9b24b20c7ec461d
Sungsan Kim <42317507+leo0842@users.noreply.github.com>
2022-09-27 22:10:08 +0900

    Reflection (#4)
    * feat: 1단계 완료
    
    * feat: 2단계 완료
    
    * feat: 3단계 완료
    
    * feat: 4단계 완료

--- reflection/src/test/java/reflection/Junit3TestRunner.java
+++ reflection/src/test/java/reflection/Junit3TestRunner.java
@@ -1,13 +1,27 @@
 package reflection;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
 
     @Test
-    void run() throws Exception {
+    void run() {
         Class<Junit3Test> clazz = Junit3Test.class;
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        Method[] declaredMethods = clazz.getDeclaredMethods();
+        Arrays.stream(declaredMethods)
+                .filter(method -> method.getName().startsWith("test"))
+                .forEach(method -> {
+                    try {
+                        method.invoke(clazz.getDeclaredConstructor().newInstance());
+                    } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException |
+                             InstantiationException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
     }
 }
--- reflection/src/test/java/reflection/Junit4TestRunner.java
+++ reflection/src/test/java/reflection/Junit4TestRunner.java
@@ -1,5 +1,8 @@
 package reflection;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 class Junit4TestRunner {
@@ -9,5 +12,19 @@ class Junit4TestRunner {
         Class<Junit4Test> clazz = Junit4Test.class;
 
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Arrays.stream(clazz.getDeclaredMethods())
+                .filter(this::findMyTestAnnotation)
+                .forEach(method -> {
+                    try {
+                        method.invoke(clazz.getDeclaredConstructor().newInstance());
+                    } catch (IllegalAccessException | InvocationTargetException | InstantiationException |
+                             NoSuchMethodException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+    }
+
+    private boolean findMyTestAnnotation(Method method) {
+        return method.getAnnotation(MyTest.class) != null;
     }
 }
