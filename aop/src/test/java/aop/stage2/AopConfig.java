804c49e4c4b17c1a32c70899ab08e41037dedc93
leokim <kunga077@gmail.com>
2022-10-16 00:04:09 +0900

    feat: 2단계 완료

--- aop/src/test/java/aop/stage2/AopConfig.java
+++ aop/src/test/java/aop/stage2/AopConfig.java
@@ -1,8 +1,41 @@
 package aop.stage2;
 
+import aop.stage1.TransactionAdvice;
+import aop.stage1.TransactionAdvisor;
+import aop.stage1.TransactionPointcut;
+import org.aopalliance.aop.Advice;
+import org.springframework.aop.Advisor;
+import org.springframework.aop.Pointcut;
+import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 public class AopConfig {
+
+    @Autowired
+    private PlatformTransactionManager platformTransactionManager;
+
+    @Bean
+    public Pointcut transactionPointcut() {
+        return new TransactionPointcut();
+    }
+
+    @Bean
+    public Advice transactionAdvice() {
+        return new TransactionAdvice(platformTransactionManager);
+    }
+
+    @Bean
+    public Advisor transactionAdvisor() {
+        return new TransactionAdvisor(transactionAdvice(), transactionPointcut());
+    }
+
+    @Bean
+    public DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator() {
+        return new DefaultAdvisorAutoProxyCreator();
+    }
 
 }
